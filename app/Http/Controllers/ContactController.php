47996cfc4fd083f1d385cbfd62a7045636cb2169
MarDi66 <yohann.mtfpro@gmail.com>
2019-12-04 16:34:36 +0100

    Send message to DB

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
     public function submit(ContactRequest $request) {
-        dd($request->input('email'));
+        $message = new Message();
+        $message->name = $request->input('name');
+        $message->email = $request->input('email');
+        $message->subject = $request->input('subject');
+        $message->message = $request->input('message');
+        $message->save();
+
+        return redirect()->route('home');
     }
 }
