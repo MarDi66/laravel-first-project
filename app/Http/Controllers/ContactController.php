6c92d131d21e7f61798df865ebbb7aec5373570c
MarDi66 <yohann.mtfpro@gmail.com>
2019-12-04 16:01:18 +0100

    Add ContactRequest validation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
-    public function submit(Request $request) {
+    public function submit(ContactRequest $request) {
         dd($request->input('email'));
     }
 }
