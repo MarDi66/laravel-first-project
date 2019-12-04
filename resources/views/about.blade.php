c134073c94ef347b570d19b4933d5777b1cd28fe
MarDi66 <yohann.mtfpro@gmail.com>
2019-12-04 13:31:23 +0100

    lorem on home + about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     <h1>About</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti rerum quaerat, dolorum nam, eius, nemo soluta cum animi quae accusamus doloremque! Temporibus a ipsum aliquam fugiat! Sit repellendus in vel. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora voluptatum quis itaque asperiores eveniet pariatur, facere labore sequi doloribus eaque veritatis nesciunt fuga a doloremque repellendus, necessitatibus, voluptates in? Quisquam.</p>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
     <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus necessitatibus a quis illo quo dolorum. Nam, eos officiis illum, illo voluptate voluptates unde quas, at aut eius aperiam hic quam.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi inventore cumque dignissimos pariatur reprehenderit aliquid amet totam repellendus aperiam, nam iure rem tenetur cum natus aspernatur illum deserunt accusamus excepturi.
+    </p>
 @endsection
 
 @section('sidebar')
     @parent
-    This is appended to the sidebar
+    <p>This is appended to the sidebar</p>
 @endsection
