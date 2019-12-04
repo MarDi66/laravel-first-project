90e8c03e87f4ea73bcb3fa0000402ed709f1a525
MarDi66 <yohann.mtfpro@gmail.com>
2019-12-04 17:11:33 +0100

    Add messages view + retrieve messages from DB

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,7 @@ Route::get('/about', function() {
     return view('about');
 })->name('about');
 
+Route::get('contact/messages', 'ContactController@getMessages')->name('get-messages');
+
 Route::post('contact/submit', 'ContactController@submit')
 ->name('contact-form-submit');
