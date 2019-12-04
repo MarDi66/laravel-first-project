28bfecf3abe2d2e7f304b39ad771b5ea69f425f4
MarDi66 <yohann.mtfpro@gmail.com>
2019-12-04 13:56:10 +0100

    Add form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,4 +2,29 @@
 
 @section('content')
     <h1>Contact us</h1>
+
+    <form>
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" name="name" id="name" placeholder="Enter name">
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+
+        <div class="form-group">
+            <label for="subject">Subject</label>
+            <input type="text" class="form-control" name="subject" id="subject" placeholder="Enter subject">
+        </div>
+
+        <div class="form-group">
+            <label for="message">Message</label>
+            <textarea type="text" class="form-control" name="message" id="message" placeholder="Enter your message" rows="3"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 @endsection
